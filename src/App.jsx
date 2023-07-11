0236a1e052b133e2dcb0cea360f2db486aa67203
Ngoy <dngoyb@gmail.com>
2023-07-11 17:11:02 +0200

    Lists added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+const initialItems = [
+	{ id: 1, description: 'Passports', quantity: 2, packed: false },
+	{ id: 2, description: 'Socks', quantity: 12, packed: true },
+	{ id: 3, description: 'Charger', quantity: 1, packed: true },
+];
+
 export default function App() {
 	return (
 		<div className='app'>
@@ -10,7 +16,7 @@ export default function App() {
 }
 
 function Logo() {
-	return <h1>🌴 Far Away </h1>;
+	return <h1> 🏝️ Far Away 🧳 </h1>;
 }
 function Form() {
 	return (
@@ -20,12 +26,32 @@ function Form() {
 	);
 }
 function ParkingList() {
-	return <div className='list'>List</div>;
+	return (
+		<div className='list'>
+			<ul>
+				{initialItems.map((item) => (
+					<Item key={item.id} item={item} />
+				))}
+			</ul>
+		</div>
+	);
+}
+
+function Item({ item }) {
+	return (
+		<li>
+			<span style={item.packed ? { textDecorationLine: 'line-through' } : {}}>
+				{item.quantity} {item.description}
+			</span>
+
+			<button>❌</button>
+		</li>
+	);
 }
 function Stats() {
 	return (
 		<footer className='stats'>
-			<em>You have X items on your list, and you already packed x (x%)</em>
+			<em>💼 You have X items on your list, and you already packed x (x%)</em>
 		</footer>
 	);
 }
