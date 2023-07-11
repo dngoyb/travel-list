0ba1e889040f670065ef958c16ce1fa6b0d47652
Ngoy <dngoyb@gmail.com>
2023-07-11 18:20:11 +0200

    Controlled input completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
+
 const initialItems = [
 	{ id: 1, description: 'Passports', quantity: 2, packed: false },
-	{ id: 2, description: 'Socks', quantity: 12, packed: true },
+	{ id: 2, description: 'Socks', quantity: 12, packed: false },
 	{ id: 3, description: 'Charger', quantity: 1, packed: true },
 ];
 
@@ -19,10 +21,31 @@ function Logo() {
 	return <h1> 🏝️ Far Away 🧳 </h1>;
 }
 function Form() {
+	const [description, setDescription] = useState('');
+	const [quantity, setQuantity] = useState(1);
+
+	function handleSubmit(e) {
+		e.preventDefault();
+	}
+
 	return (
-		<div className='add-form'>
+		<form className='add-form' onSubmit={handleSubmit}>
 			<h3>What do you need for your trip?</h3>
-		</div>
+			<select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+				{Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+					<option value={num} key={num}>
+						{num}
+					</option>
+				))}
+			</select>
+			<input
+				type='text'
+				placeholder='Item...'
+				value={description}
+				onChange={(e) => setDescription(e.target.value)}
+			/>
+			<button>Add</button>
+		</form>
 	);
 }
 function ParkingList() {
